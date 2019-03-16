b76ed222e3e3a367e3b4537e72833e21ede051b6
Samuel Isaacson <isaacsas@users.noreply.github.com>
2019-03-16 10:03:21 -0400

    tutorial II updates

--- script/models/diffeqbio_II_networkproperties.jl
+++ script/models/diffeqbio_II_networkproperties.jl
@@ -51,3 +51,52 @@ findall(depset -> in(:m₁, depset), dependents.(rn, 1:numreactions(rn)))
 
 rxtorx_depgraph(rn)
 
+
+rnmin = @min_reaction_network begin
+    (δ,γ), m₁ ↔ ∅
+    (δ,γ), m₂ ↔ ∅
+    β, m₁ --> m₁ + P₁
+    β, m₂ --> m₂ + P₂
+    μ, P₁ --> ∅
+    μ, P₂ --> ∅
+end δ γ β μ;
+
+
+addspecies!(rnmin, :D₁)
+addspecies!(rnmin, :D₂)
+addspecies!(rnmin, :T)
+
+
+addparam!(rnmin, :α)
+addparam!(rnmin, :K)
+addparam!(rnmin, :n)
+addparam!(rnmin, :k₊)
+addparam!(rnmin, :k₋)
+
+
+addreaction!(rnmin, :(hillr(D₁,α,K,n)), :(∅ --> m₂))
+addreaction!(rnmin, :((k₊,k₋)), :(2P₂ ↔ D₂))
+addreaction!(rnmin, :k₊, :(2P₁ --> D₁))
+addreaction!(rnmin, :k₋, :(D₁ --> 2P₁))
+
+
+addreaction!(rnmin, :(hillr(D₂,α,K,n)), (), (:m₁ => 1,))
+addreaction!(rnmin, :k₊, (:P₁=>1, :P₂=>1), (:T=>1,))
+addreaction!(rnmin, :k₋, (:T=>1,), (:P₁=>1, :P₂=>1))
+
+
+setdiff(species(rn), species(rnmin))
+
+
+setdiff(params(rn), params(rnmin))
+
+
+rxidx = numreactions(rn)
+setdiff(substrates(rn, rxidx), substrates(rnmin, rxidx))
+
+
+setdiff(products(rn, rxidx), products(rnmin, rxidx))
+
+
+rateexpr(rn, rxidx) == rateexpr(rnmin, rxidx)
+
