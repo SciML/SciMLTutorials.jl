b69dc9049627c142ad44f302ef1c11a402ce1e0e
Chris Rackauckas <accounts@chrisrackauckas.com>
2019-06-21 08:57:58 -0400

    add new weaved Bayesian

--- script/models/pendulum_bayesian_inference.jl
+++ script/models/pendulum_bayesian_inference.jl
@@ -1,15 +1,17 @@
 
-using DiffEqBayes, OrdinaryDiffEq, ParameterizedFunctions, RecursiveArrayTools, Distributions, Plots
+using DiffEqBayes, OrdinaryDiffEq, RecursiveArrayTools, Distributions, Plots, StatsPlots
 
 
-f1 = @ode_def begin
-        dx = y
-        dy = -(g/L)*sin(x)
-    end g L
+function pendulum(du,u,p,t)
+    ω,L = p
+    x,y = u
+    du[1] = ω*y
+    du[2] = -(9.8/L)*sin(x)
+end
 
 u0 = [1.0,0.1]
 tspan = (0.0,10.0)
-prob1 = ODEProblem(f1,u0,tspan,[9.79, 1.0])
+prob1 = ODEProblem(pendulum,u0,tspan,[1.0,2.5])
 
 
 sol = solve(prob1,Tsit5())
@@ -24,8 +26,14 @@ data = convert(Array,randomized)
 scatter!(data')
 
 
-priors = [Uniform(9.5,10.0), Uniform(0.9,1.1)]
+priors = [Uniform(0.1,3.0), Normal(3.0,1.0)]
 
 
-bayesian_result = turing_inference(prob1,Tsit5(),t,data,priors;num_samples=15000)
+bayesian_result = turing_inference(prob1,Tsit5(),t,data,priors;num_samples=10_000)
+
+
+plot(bayesian_result)
+
+
+plot(bayesian_result, colordim = :parameter)
 
