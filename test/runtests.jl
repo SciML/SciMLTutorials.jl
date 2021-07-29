739886495ef0f784f20c51c8ff3c81b612329b9f
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2021-07-29 06:58:10 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,3 @@
 using SciMLTutorials
-SciMLTutorials.weave_file("Testing","test.jmd")
+tutorials_dir = joinpath(dirname(@__DIR__), "tutorials")
+SciMLTutorials.weave_file(joinpath(tutorials_dir, "Testing"), "test.jmd")
